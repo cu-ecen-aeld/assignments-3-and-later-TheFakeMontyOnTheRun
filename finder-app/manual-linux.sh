bf0fd5b119bd21a93a660cc07dfc2661c9fdf259
Daniel Monteiro <monteiroquiet@gmail.com>
2025-11-21 19:09:07 +0000

    feat: Linux kernel and filesystem booting!
    Sadly, the assignment checker is still failing...

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -26,33 +26,36 @@ mkdir -p ${OUTDIR}
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
-	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+    echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+    git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
-    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
-    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
-    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs
+    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
+    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
+    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
+    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+    echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
 mkdir -p "$OUTDIR"/rootfs
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image.gz ${OUTDIR}/rootfs/zImage
+
 cd "$OUTDIR"/rootfs
-mkdir -p bin sbin etc proc sys dev lib lib64 tmp usr var
+mkdir -p bin sbin etc proc sys dev lib lib64 tmp usr var home
 mkdir -p usr/bin usr/lib usr/sbin
 mkdir -p var/log
 cd "$OUTDIR"
@@ -64,27 +67,44 @@ git clone git://busybox.net/busybox.git
     git checkout ${BUSYBOX_VERSION}
     make distclean
     make defconfig
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-    make CONFIG_PREFIX="$OUTDIR"/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX="$OUTDIR"/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
 
-# TODO: Make device nodes
+cp $(aarch64-none-linux-gnu-gcc --print-sysroot)/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+cp $(aarch64-none-linux-gnu-gcc --print-sysroot)/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp $(aarch64-none-linux-gnu-gcc --print-sysroot)/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp $(aarch64-none-linux-gnu-gcc --print-sysroot)/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
 
-# TODO: Clean and build the writer utility
 
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
 
-# TODO: Chown the root directory
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=aarch64-none-linux-gnu-
 
-# TODO: Create initramfs.cpio.gz
+cp writer  ${OUTDIR}/rootfs/home
+cp autorun-qemu.sh  ${OUTDIR}/rootfs/home
+cp finder-test.sh  ${OUTDIR}/rootfs/home
+
+mkdir -p ${OUTDIR}/rootfs/home/conf
+cp -R ../conf/*  ${OUTDIR}/rootfs/home/conf
+
+cd ${OUTDIR}
+sudo chown -R root:root rootfs
+
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
