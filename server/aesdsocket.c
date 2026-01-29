f1f74646840a10981447c282bed8c538b376085f
Daniel Monteiro <monteiroquiet@gmail.com>
2026-01-29 20:44:46 +0000

    Running in an infinite loop, handling signals for quitting

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,3 +1,4 @@
+#include <signal.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -6,6 +7,16 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+
+/* general flag for stopping and cleaning up */
+int running = 1;
+
+void handleSignals(){
+    if (running) {
+        syslog(LOG_DEBUG, "Caught signal, exiting");
+        running = 0;
+    }
+}
 
 /**
  * lifted from a game I wrote a couple of years ago. It kind of sucked tho.
@@ -58,9 +69,14 @@ int createAcceptingSocket()
     return sockfd;
 }
 
-
 int main(int argc, char** argv)
 {
+    /* Setup signal handler */
+    struct sigaction handler;
+    handler.sa_handler = handleSignals;
+    sigaction(SIGINT, &handler, NULL);
+    sigaction(SIGTERM, &handler, NULL);
+
     /* prepare the data repository */
     FILE *output;
     output = fopen("/var/tmp/aesdsocketdata", "a+");
@@ -70,51 +86,53 @@ int main(int argc, char** argv)
     socklen_t clilen;
     struct sockaddr_in cli_addr;
     clilen = sizeof(cli_addr);
-    int newsockfd = accept(sock, (struct sockaddr*)&cli_addr, &clilen);
-    char ipstr[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &cli_addr.sin_addr, ipstr, sizeof ipstr);
+    while ( running )
+    {
+        int newsockfd = accept(sock, (struct sockaddr*)&cli_addr, &clilen);
+        char ipstr[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &cli_addr.sin_addr, ipstr, sizeof ipstr);
 
-    syslog(LOG_DEBUG, "Accepted connection from %s", ipstr);
+        syslog(LOG_DEBUG, "Accepted connection from %s", ipstr);
 
-    ssize_t bytesRead = 0;
-    do {
-        char buffer[257];
-        memset(buffer, 0, 257);
-        bytesRead = recv(newsockfd, buffer, 256, MSG_DONTWAIT);
-        if (bytesRead > 0) {
-            /* append the received data */
-            fwrite(&buffer, bytesRead, 1, output);
-            buffer[0] = '\n';
-            fwrite(&buffer, 1, 1, output);
+        ssize_t bytesRead = 0;
+        do {
+            char buffer[257];
+            memset(buffer, 0, 257);
+            bytesRead = recv(newsockfd, buffer, 256, MSG_DONTWAIT);
+            if (bytesRead > 0) {
+                /* append the received data */
+                fwrite(&buffer, bytesRead, 1, output);
+                buffer[0] = '\n';
+                fwrite(&buffer, 1, 1, output);
+            }
+        } while (bytesRead > 0);
+
+        /* fully read the existing data */
+        char *currentBuffer = NULL;
+        FILE* data = fopen("/var/tmp/aesdsocketdata", "r");
+        fseek(data, 0, SEEK_END);
+        size_t size = ftell(data) + 1;
+        currentBuffer = (char*)malloc(size);
+        rewind(data);
+        fread(currentBuffer, 1, size, data);
+        currentBuffer[size - 1] = 0;
+
+        /* send what we have back */
+        ssize_t offset = 0;
+        while (offset < size) {
+            offset += send(newsockfd, currentBuffer + offset, size - offset, 0);
         }
-    } while (bytesRead > 0);
 
-    /* fully read the existing data */
-    char *currentBuffer = NULL;
-    FILE* data = fopen("/var/tmp/aesdsocketdata", "r");
-    fseek(data, 0, SEEK_END);
-    size_t size = ftell(data) + 1;
-    currentBuffer = (char*)malloc(size);
-    rewind(data);
-    fread(currentBuffer, 1, size, data);
-    currentBuffer[size - 1] = 0;
+        syslog(LOG_DEBUG, "Closed connection from %s", ipstr);
 
-    /* send what we have back */
-    ssize_t offset = 0;
-    while (offset < size) {
-        offset += send(newsockfd, currentBuffer + offset, size - offset, 0);
+        /* clean up for this peer */
+        free(currentBuffer);
+        fclose(data);
+        close(newsockfd);
     }
 
-    syslog(LOG_DEBUG, "Closed connection from %s", ipstr);
-
-    /* clean up */
-    free(currentBuffer);
-    fclose(data);
-
-
-    /* clean up */
+    /* clean up server */
     fclose(output);
-    close(newsockfd);
     close(sock);
 
     return 0;
