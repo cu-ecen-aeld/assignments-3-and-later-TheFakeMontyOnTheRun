50691d82f1393a795a6345ff0663415b856a02bc
Daniel Monteiro <monteiroquiet@gmail.com>
2026-01-30 21:33:31 +0000

    fix: daemon stability

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -16,8 +16,10 @@ int running = 1;
 /* are we runnning as daemon? */
 int runAsDaemon = 0;
 
-void handleSignals(){
-    if (running) {
+void handleSignals()
+{
+    if (running)
+    {
         syslog(LOG_DEBUG, "Caught signal, exiting");
         running = 0;
     }
@@ -28,11 +30,11 @@ void handleSignals(){
  * I had to modify it in a few spots and might reflect those improvements back into the game :)
  * @return new socket, ready for accepting connections
  */
-int createAcceptingSocket() {
+int createAcceptingSocket()
+{
     int sockfd, portno;
 
     struct sockaddr_in serv_addr;
-    int n;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -64,27 +66,31 @@ int createAcceptingSocket() {
     return sockfd;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
     /* Setup signal handler */
     handler.sa_handler = handleSignals;
     sigaction(SIGINT, &handler, NULL);
     sigaction(SIGTERM, &handler, NULL);
 
-    if (argc > 1 && strcmp(argv[1], "-d") == 0) {
+    if (argc > 1 && strcmp(argv[1], "-d") == 0)
+    {
         runAsDaemon = 1;
     }
 
-    FILE *output;
+    FILE* output;
 
     /* Accept incoming connections */
     int sock = createAcceptingSocket();
 
-    if (sock == -1) {
+    if (sock == -1)
+    {
         syslog(LOG_ERR, "Could not create accepting socket");
         return -1;
     }
 
-    if (listen(sock, 5) < 0) {
+    if (listen(sock, 5) < 0)
+    {
         syslog(LOG_ERR, "Could not listen on listening socket");
         close(sock);
         running = 0;
@@ -94,50 +100,61 @@ int main(int argc, char** argv) {
     socklen_t clilen;
     struct sockaddr_in cli_addr;
     clilen = sizeof(cli_addr);
-    if (!runAsDaemon || !fork()) {
-        while ( running ) {
-            int newsockfd = accept(sock, (struct sockaddr*)&cli_addr, &clilen);
-            char ipstr[INET_ADDRSTRLEN];
-            inet_ntop(AF_INET, &cli_addr.sin_addr, ipstr, sizeof ipstr);
 
-            syslog(LOG_DEBUG, "Accepted connection from %s", ipstr);
-            ssize_t bytesRead = 0;
-
-            output = fopen("/var/tmp/aesdsocketdata", "a+");
-            do {
-                char buffer[256];
-                memset(buffer, 0, 256);
-                bytesRead = recv(newsockfd, buffer, 256, MSG_DONTWAIT);
-                if (bytesRead > 0) {
-                    /* append the received data */
-                    fwrite(&buffer, 1, bytesRead, output);
-                }
-            } while (bytesRead > 0);
-            fclose(output);
-
-            /* fully read the existing data */
-            char *currentBuffer = NULL;
-            FILE* data = fopen("/var/tmp/aesdsocketdata", "r");
-            fseek(data, 0, SEEK_END);
-            size_t size = ftell(data);
-            currentBuffer = (char*)malloc(size);
-            rewind(data);
-            fread(currentBuffer, 1, size, data);
-
-            /* send what we have back */
-            ssize_t offset = 0;
-            while (offset < size) {
-                offset += send(newsockfd, currentBuffer + offset, size - offset, 0);
-            }
-
-            syslog(LOG_DEBUG, "Closed connection from %s", ipstr);
-
-            /* clean up for this peer */
-            free(currentBuffer);
-            fclose(data);
-            close(newsockfd);
+    if (runAsDaemon) {
+        if (fork()) {
+            return 0;
         }
     }
+
+    while (running)
+    {
+        int newsockfd = accept(sock, (struct sockaddr*)&cli_addr, &clilen);
+        char ipstr[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &cli_addr.sin_addr, ipstr, sizeof ipstr);
+
+        syslog(LOG_DEBUG, "Accepted connection from %s", ipstr);
+        ssize_t bytesRead = 0;
+
+        output = fopen("/var/tmp/aesdsocketdata", "a+");
+        do
+        {
+            char buffer[256];
+            memset(buffer, 0, 256);
+            bytesRead = recv(newsockfd, buffer, 256, MSG_DONTWAIT);
+            if (bytesRead > 0)
+            {
+                /* append the received data */
+                fwrite(&buffer, 1, bytesRead, output);
+            }
+        }
+        while (bytesRead > 0);
+        fclose(output);
+
+        /* fully read the existing data */
+        char* currentBuffer = NULL;
+        FILE* data = fopen("/var/tmp/aesdsocketdata", "r");
+        fseek(data, 0, SEEK_END);
+        size_t size = ftell(data);
+        currentBuffer = (char*)malloc(size);
+        rewind(data);
+        fread(currentBuffer, 1, size, data);
+
+        /* send what we have back */
+        ssize_t offset = 0;
+        while (offset < size)
+        {
+            offset += send(newsockfd, currentBuffer + offset, size - offset, 0);
+        }
+
+        syslog(LOG_DEBUG, "Closed connection from %s", ipstr);
+
+        /* clean up for this peer */
+        free(currentBuffer);
+        fclose(data);
+        close(newsockfd);
+    }
+
 
     /* clean up server */
     unlink("/var/tmp/aesdsocketdata");
